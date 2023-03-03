6c4f4be48bdfd6a16fc9c551ed23e25ff2244e6b
Hayden coates <haydencoates837@gmail.com>
2023-03-03 11:13:30 -0800

    Added pin 11 as output - Led 11 and 12 are working

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -31,17 +31,24 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(12, HIGH);   // turn pin 12 to high so it will turn on
+  digitalWrite(11, HIGH);   // turn pin 11 to high so it will turn on
   delay(1000);                       // wait for a second
-  digitalWrite(12, LOW);    //  turn pin 12 to low so it will turn off 
+  digitalWrite(12, LOW);    //  turn pin 12 to low so it will turn off
+  digitalWrite(11, LOW);    //  turn pin 12 to low so it will turn off 
   delay(1000);                       // wait for a second
 
   digitalWrite(12, HIGH);   // turn pin 12 to high so it will turn on
+  digitalWrite(11, HIGH);   // turn pin 12 to high so it will turn on
   delay(1500);                       // wait for a second and a half
   digitalWrite(12, LOW);    //  turn pin 12 to low so it will turn off 
+  digitalWrite(11, LOW);    //  turn pin 12 to low so it will turn off 
   delay(2000);                       // wait for two second
+
+  
 }
